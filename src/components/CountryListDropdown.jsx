3ca1dcb6a704275b2139ae92cb970abacfb74363
Niklas Bergh <nollis@icloud.com>
2022-12-19 23:29:46 +0100

    updated functions
    dropdown

--- src/components/CountryListDropdown.jsx
+++ src/components/CountryListDropdown.jsx
@@ -1,47 +1,65 @@
 import React from 'react'
 import axios from 'axios';
-export const CountryDropdown = (props) => (
-  <div className="form-group">
-    <strong>{props.CountryName}</strong>
-    <select
-      className="form-control"
-      name="{props.CountryName}"
-      onChange={props.onChange}
-    >
-      <option defaultValue>Select {props.CountryName}</option>
-      {props.options.map((item, index) => (
-        <option key={index} value={item.id}>
-          {item.CountryName}
-        </option>
-      ))}
-    </select>
-  </div>
-)
-export default class CountryListDropDown extends React.Component {
-  constructor() {
-    super()
+
+export default class CountryDropDown extends React.Component {
+  constructor(props) {
+    super(props)
     this.state = {
-      collection: [],
-      value: '',
+      CountryId: '',
+      CountryData: [],
+      CityData: [],
+      LanguageData: []
     }
   }
-  componentDidMount() {
-    axios.get("https://localhost:7184/api/react/countries")
-      .then((response) => response.json())
-      .then((res) => this.setState({ collection: res }))
-  }
+  componentDidMount() {  
+    axios.get('https://localhost:7184/api/react/countries').then(response => {  
+            console.log(response.data);  
+            this.setState({  
+              CountryData: response.data  
+            });  
+    });
+    axios.get('https://localhost:7184/api/react/languages').then(res => {  
+            console.log(res.data);  
+            this.setState({  
+              LanguageData: res.data  
+            });  
+    });  
+  } 
+
+  ChangeCity = (e) => {  
+    this.setState({ StateId: e.target.value });  
+    axios.get('https://localhost:7184/api/react/cities?CountryId=' + e.target.value).then(response => {  
+            console.log(response.data);  
+            this.setState({  
+                    CityData: response.data  
+            });  
+    });  
+  }  
+
   onChange = (event) => {
     this.setState({ value: event.target.value })
   }
   render() {
     return (
       <div className="container mt-4">
-        <h2>React Dropdown List with Bootstrap Example</h2>
-        <CountryDropdown
-          name={this.state.CountryName}
-          options={this.state.collection}
-          onChange={this.onChange}
-        />
+        <select className="form-control" name="country" value={this.state.CountryId} onChange={this.ChangeCity} >  
+          <option>Select Country</option>  
+          {this.state.CountryData.map((e, key) => {  
+                  return <option key={key} value={e.countryId}>{e.countryName}</option>;  
+          })}  
+        </select>
+        <select className="form-control" name="city" value={this.state.CityData} >  
+          <option>Select City</option>  
+          {this.state.CityData.map((e, key) => {  
+                  return <option key={key} value={e.cityId}>{e.cityName}</option>;  
+          })}  
+        </select>
+        <select className="form-control" name="language" value={this.state.LanguageData} >  
+          <option>Select Language</option>  
+          {this.state.LanguageData.map((e, key) => {  
+                  return <option key={key} value={e.id}>{e.languageName}</option>;  
+          })}  
+        </select> 
       </div>
     )
   }
